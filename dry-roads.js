a51a034e562cb95a4decb045438159e4c5113b2a
bGauraang <gauraang.bhartiya@teachforindia.org>
2019-02-12 05:49:00 +0530

    ErrorHandler

--- dry-roads.js
+++ dry-roads.js
@@ -2,4 +2,5 @@ module.exports = {
 	accountsEndpoints: require('./lib/crossroads-accounts-endpoints'),
 	tasks: require('./lib/tasks'),
 	utils: require('./lib/utils'),
+	ErrorHandler: require('./lib/ErrorHandler'),
 }
